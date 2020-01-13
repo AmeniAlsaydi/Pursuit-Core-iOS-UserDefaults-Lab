d0b0f9bcf67bd46062eaefaeecf2dbc708770352
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-01-13 16:06:08 -0500

    created pickerView

---
+++ User_Defaults/User_Defaults/LaunchViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  User_Defaults
+//
+//  Created by Amy Alsaydi on 1/13/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    
+    @IBOutlet weak var textFeild: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        pickerData = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
+    }
+
+
+}
+
+extension LaunchViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    
+}
+
