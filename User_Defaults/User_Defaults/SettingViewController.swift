ede43d22aaf2fe41bc090107c774f69f0d453814
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-01-14 16:30:57 -0500

    created custom date picker

--- User_Defaults/User_Defaults/SettingViewController.swift
+++ User_Defaults/User_Defaults/SettingViewController.swift
@@ -18,12 +18,15 @@ protocol SettingDelegate {
 class SettingViewController: UIViewController {
     
     @IBOutlet weak var textFeild: UITextField!
-    @IBOutlet weak var pickerView: UIPickerView!
-    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var signPickerView: UIPickerView!
+    @IBOutlet weak var customDatePicker: UIPickerView!
     
     var pickerData: [String] = [String]()
-    
     var settingsDelegate: SettingDelegate?
+    
+    var months: Array<String> = Calendar.current.monthSymbols
+    var daysArray = Array(1...31)
+    
     
     var sign = "gemini" {
         didSet {
@@ -34,107 +37,105 @@ class SettingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        pickerView.dataSource = self
-        pickerView.delegate = self
+        signPickerView.dataSource = self
+        signPickerView.delegate = self
+        customDatePicker.delegate = self
+        customDatePicker.dataSource = self
         
         textFeild.delegate = self
         
-        datePicker.datePickerMode = .date
+        // datePicker.datePickerMode = .date
         
         pickerData = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
     }
     
-    func getSign(date: String){
-        
-        // date in this string format "dd.MM"
-        
-        let dateParts = date.components(separatedBy: ".")
-        
-        let day = Int(dateParts[0]) ?? 1
-        let month = Int(dateParts[1]) ?? 1
-        
-        if month == 3 && (21...31).contains(day) || month == 4 && (1...19).contains(day) {
-            sign = "aries"
-        }
-        if month == 4 && (20...30).contains(day) || month == 5 && (1...20).contains(day) {
-            sign = "taurus"
-        }
-        
-        if month == 5 && (21...31).contains(day) || month == 6 && (1...20).contains(day) {
-            sign = "gemini"
-        }
-        
-        if month == 6 && (21...30).contains(day) || month == 7 && (1...22).contains(day) {
-            sign = "cancer"
-        }
-        
-        if month == 7 && (23...31).contains(day) || month == 8 && (1...22).contains(day) {
-            sign = "leo"
-        }
-        if month == 8 && (23...31).contains(day) || month == 9 && (1...22).contains(day) {
-            sign = "virgo"
-        }
-        
-        if month == 9 && (23...31).contains(day) || month == 10 && (1...22).contains(day) {
-            sign = "libra"
-        }
-        
-        if month == 10 && (23...31).contains(day) || month == 11 && (1...21).contains(day) {
-            sign = "scorpio"
-        }
-        
-        if month == 11 && (22...31).contains(day) || month == 12 && (1...21).contains(day) {
-            sign = "sagittarius"
-        }
-        
-        if month == 12 && (22...31).contains(day) || month == 1 && (1...19).contains(day) {
-            sign = "capricorn"
-        }
-        if month == 1 && (20...31).contains(day) || month == 2 && (1...18).contains(day) {
-            sign = "aquarius"
-        }
-        
-        if month == 2 && (19...31).contains(day) || month == 3 && (1...20).contains(day) {
-            sign = "pisces"
-        }
-        
-    }
-    
     
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
-      
+        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM"
         let date = dateFormatter.string(from: sender.date)
-        //print(date)
-        
-        getSign(date: date)
+        print(date)
         
         
     }
     
-
+    
 }
 
 extension SettingViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        1
+        if signPickerView == pickerView {
+            return 1
+        } else if pickerView == customDatePicker {
+            return 2
+        }
+        
+        return 1
     }
     
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
+        
+        if pickerView == signPickerView {
+            return pickerData.count
+        }
+        if pickerView == customDatePicker {
+            if component == 0 {
+                return daysArray.count
+            }
+            if component == 1 {
+                return 12
+
+            }
+        }
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == signPickerView {
+            return pickerData[row]
+        }
+            
+        else if pickerView == customDatePicker {
+            if component == 0{
+                return  daysArray[row].description
+            } else {
+                return months[row]
+            }
+        }
+        
         return pickerData[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == signPickerView {
+            sign = pickerData[row].lowercased() as String
+        } else if pickerView == customDatePicker {
+            print("component\(component), row:\(row)")
+            var str = ""
+            if component == 0 {
+                str += "Day \(daysArray[row])"
+            } else if component == 1 {
+                str += "Month \(months[row])"
+            }
+            print(str)
+            
+        }
         
-        sign = pickerData[row].lowercased() as String
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        if pickerView == customDatePicker {
+            if component == 0 {
+                return 45
+            }
+            return 140
+        }
+        
+        return 414
         
     }
     
@@ -149,3 +150,5 @@ extension SettingViewController: UITextFieldDelegate {
     }
     
 }
+
+
